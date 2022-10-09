e694bb8018c9be9c1b91a36c59bdc0e31dad8538
moinur-rahman <rahmanmoin27@gmail.com>
2022-10-10 00:09:29 +0600

    Express typescript setup

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express } from "express";
+
+const server = async () => {
+  const app: Express = express();
+
+  app.get("/", (_, res) => {
+    res.send("Hello World");
+  });
+
+  const port = process.env.port || 5000;
+  app.listen(port, () => console.log(`Server started on port ${port}`));
+};
+
+server().catch((error) => console.log(error));
+
+console.log("Hello world");
